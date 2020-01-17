c67e3d1172bfd9e4424cae1ee58b6978b51c1765
safoex <safoex@hotmail.com>
2020-01-17 15:24:15 +0300

    now each manip know his angle

--- manipulator_class.py
+++ manipulator_class.py
@@ -14,9 +14,15 @@ class Manipulator():
         return((self.plane_num - 1) * self.n_manips + (self.rel_manip_num - 1))
     def get_manip_angle(self):
         base = -15
-        #print (self.manip_center_angle * 2 * ((self.rel_manip_num + 1) % 2))
-        print(base + ((self.plane_num - 1) * self.between_plane_angle + self.manip_center_angle * 2 * ((self.rel_manip_num + 1) % 2)))
-        return(base + (self.plane_num - 1) * self.between_plane_angle + self.manip_center_angle * 2 * ((self.rel_manip_num + 1) % 2)) 
+        if (self.rel_manip_num % 2 == 0):
+            plane_even = 1
+        else:
+            plane_even = 0
+        print((self.manip_center_angle * 2 + self.between_plane_angle) * (self.plane_num - 1) + base + plane_even * self.manip_center_angle * 2)
+        get_angle = (self.manip_center_angle * 2 + self.between_plane_angle) * (self.plane_num - 1) + base + plane_even * self.manip_center_angle * 2
+        if get_angle < 0:
+            get_angle = 360 + get_angle
+        return(get_angle) 
     def execute(self):
         print("---manip " + str(self.abs_manip_num) + " (plane " + str(self.plane_num) + " manip " + str(self.rel_manip_num) + ")")
         print("----angle of manip: " + str(self.manip_angle))
